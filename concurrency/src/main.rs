2d6add3c44144323b881e8122efb14346940b2b6
bits0x <surisriva1999@gmail.com>
2024-02-02 16:17:25 +0530

    feat: using message passing to transfer data b/w threads

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,8 +1,9 @@
 use std::thread;
 use std::time::Duration;
+use std::sync::mpsc;
 
 fn main() {
-
+    // Concurrency : Creating threads
     let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("{} in spawn thread", i);
@@ -25,5 +26,43 @@ fn main() {
     });
 
     handle2.join().unwrap();
+
+    //----------------------------------------------------
+    // Concurrency : Message passing
+    let (tx, rx) = mpsc::channel(); 
+
+    let tx1 = tx.clone(); // creating another transmitter channel
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
 
 }

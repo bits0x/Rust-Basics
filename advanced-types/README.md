35047a66d80698ba285fc4485a3a4f99545668b6
bits0x <surisriva1999@gmail.com>
2024-02-06 20:52:00 +0530

    feat: add code examples for advanced types in rust

---
+++ advanced-types/README.md
@@ -0,0 +1,5 @@
+# Advanced Types
+
+This directory contains a simple program to understand advanced types used in rust
+
+Use `cargo run` to run the program.
---
+++ advanced-types/src/main.rs
@@ -0,0 +1,18 @@
+pub fn main() {
+    // Creating Type Synonyms with Type Aliases
+    type Kilometers = i32;
+
+    let x: i32 = 5;
+    let y: Kilometers = 5;
+
+    println!("x + y = {}", x + y);
+
+    type Thunk = Box<dyn Fn() + Send + 'static>;
+
+    let f: Thunk = Box::new(|| println!("hi"));
+
+    fn takes_long_type(f: Thunk) {
+        // --snip--
+    }
+
+}

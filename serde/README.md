70d29464378dc5b282ca1fd450f458a769b5ce66
bits0x <surisriva1999@gmail.com>
2024-02-08 13:59:47 +0530

    feat: add code examples to desrialize json data to rust type using serde

---
+++ serde/README.md
@@ -0,0 +1,5 @@
+# Serde 
+
+This directory contains a simple program to understand parsing json in rust using serde and reqwest
+
+Use `cargo run` to run the program.
---
+++ serde/src/main.rs
@@ -0,0 +1,24 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Todo {
+    #[serde(rename = "userId")]
+    user_id: i32,
+    id: Option<i32>,
+    title: String,
+    completed: bool
+}
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    // Deserialize json into rust type
+    let todos: Vec<Todo> = reqwest::Client::new()
+        .get("https://jsonplaceholder.typicode.com/todos?userId=1")
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    println!("{:#?}", todos);
+    Ok(())
+}
